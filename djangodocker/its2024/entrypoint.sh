3c04fcddd9ec3337b98c071bc5a33ab2e588291f
octalucero <octaviolucero2005@gmail.com>
2024-03-20 12:00:23 -0300

    TP1

---
+++ djangodocker/its2024/entrypoint.sh
@@ -0,0 +1,10 @@
+    #!/bin/sh
+
+    echo "Waiting for db to be ready... "
+    sleep 10 
+    echo "Continue..."
+
+    python manage.py migrate
+
+    echo "Starting django..."
+    python manage.py runserver 0.0.0.0:8000
